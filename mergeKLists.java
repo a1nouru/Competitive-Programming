ba20fbcfe4e509ecf37efcef73191902f010f9aa
Nouru Muneza <nizzogentl@gmail.com>
2018-08-25 11:34:33 +0200

    Merge k Sorted Lists (Java)

---
+++ mergeKLists.java
@@ -0,0 +1,42 @@
+public class Main {
+
+    public class ListNode{
+        int val;
+        ListNode next;
+
+        public ListNode(int val){
+            this.val = val;
+        }
+    }
+
+    public class solution{
+
+        public ListNode mergeKLists(List<ListNode> lists){
+
+            if(lists.size() == 0 || lists == null){
+                return null;
+            }
+            PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.size(), new Comparator<ListNode>() {
+                @Override
+                public int compare(ListNode o1, ListNode o2) {
+                    return (o1.val - o2.val);
+                }
+            });
+            // add all listNodes from Lists to the queue.  
+            for (ListNode node: lists){
+               if(node != null) queue.add(node);
+            }
+            ListNode dummyHead = new ListNode(0);
+            ListNode p = dummyHead;
+
+            //In this iteration, I remove the smallest ListNode and add it to the head final listNode 
+            while (!queue.isEmpty()) {
+                ListNode temp = queue.poll();
+                p.next = temp;
+                if (temp.next != null) queue.add(temp.next);
+                p = p.next;
+            }
+            return dummyHead.next;
+        }
+    }
+}
