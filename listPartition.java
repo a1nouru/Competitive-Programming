aad2fe0151f303ed68dbba0de50df0d4d5a5fd9f
Nouru Muneza <nizzogentl@gmail.com>
2018-09-18 15:12:10 -0700

    86. Partition List

---
+++ listPartition.java
@@ -0,0 +1,54 @@
+/*
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Example:
+
+Input: head = 1->4->3->2->5->2, x = 3
+Output: 1->2->2->4->3->5
+*/
+
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if(head == null) return null;
+ 
+        ListNode fakeHead1 = new ListNode(0);
+        ListNode fakeHead2 = new ListNode(0);
+        fakeHead1.next = head;
+ 
+        ListNode p = head;
+        ListNode prev = fakeHead1;
+        ListNode p2 = fakeHead2;
+ 
+        while(p != null){
+            if(p.val < x){
+                p = p.next;
+                prev = prev.next;
+            }else{
+ 
+                p2.next = p;
+                prev.next = p.next;
+ 
+                p = prev.next;
+                p2 = p2.next;
+            } 
+        }
+ 
+        // close the list
+        p2.next = null;
+ 
+        prev.next = fakeHead2.next;
+ 
+        return fakeHead1.next;
+    }
+}
+
+
+/*
+Another way you could look at this is, by creating two dummy nodes as before but then judging if it's greater or smaller. 
+If greater i toges to the first dummy node if smaller it goes to the second node and then you can intersect the 
+two dummy nodes later after your done traversing the list. 
+
+Have FUN!!!!
+*/
