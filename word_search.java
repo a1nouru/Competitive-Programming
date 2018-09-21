a324609b2b6e587c86b5a10daf1b665521627bb0
Nouru Muneza <nizzogentl@gmail.com>
2018-09-21 13:05:34 -0700

    79. Word Search

---
+++ word_search.java
@@ -0,0 +1,31 @@
+public static boolean exist(char [][] arr, String word){
+        if (arr.length == 0 && arr[0].length == 0) return false; // base case
+
+        int m = arr.length;
+        int n = arr[0].length;
+
+        for (int i = 0; i< m; i++){
+            for (int j = 0; j < n; j++){
+                if (DFS(arr,word,i,j,0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean DFS(char[][] arr,String word, int i, int j, int k){
+        int m = arr.length;
+        int n = arr[0].length;
+
+        if (i < 0 || j < 0 || i > m || j> n) //checking if out of bounds
+            return false;
+        if (arr[i][j] == word.charAt(k)){
+            if (k == word.length()-1) // we are at the end of the word and all the characters have been found.
+                return true;
+            else if( DFS(arr,word,i-1,j,k) //going left
+                || DFS(arr,word,i+1,j,k) //going right
+                || DFS(arr,word,i,j-1,k) //going down
+                || DFS(arr,word,i,j+1,k)) //going up
+                return true;
+        }
+        return false;
+    }
