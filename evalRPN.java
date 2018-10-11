0df8ad725ebecbcc1ff3e04fdf0ba8c50be90e5e
Nouru Muneza <nizzogentl@gmail.com>
2018-10-11 10:40:48 -0700

    150. Evaluate Reverse Polish Notation

---
+++ evalRPN.java
@@ -0,0 +1,74 @@
+/*
+
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Note:
+
+Division between two integers should truncate toward zero.
+The given RPN expression is always valid. That means the expression would always evaluate to a result and there won't be any divide by zero operation.
+Example 1:
+
+Input: ["2", "1", "+", "3", "*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: ["4", "13", "5", "/", "+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+Output: 22
+Explanation: 
+  ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+
+*/
+
+public class Solution {
+    public int evalRPN(String[] tokens) {
+ 
+        int returnValue = 0;
+ 
+        String operators = "+-*/";
+ 
+        Stack<String> stack = new Stack<String>();
+ 
+        for(String t : tokens){
+            if(!operators.contains(t)){
+                stack.push(t);
+            }else{
+                int a = Integer.valueOf(stack.pop());
+                int b = Integer.valueOf(stack.pop());
+                int index = operators.indexOf(t);
+                switch(index){
+                    case 0:
+                        stack.push(String.valueOf(a+b));
+                        break;
+                    case 1:
+                        stack.push(String.valueOf(b-a));
+                        break;
+                    case 2:
+                        stack.push(String.valueOf(a*b));
+                        break;
+                    case 3:
+                        stack.push(String.valueOf(b/a));
+                        break;
+                }
+            }
+        }
+ 
+        returnValue = Integer.valueOf(stack.pop());
+ 
+        return returnValue;
+ 
+    }
+}
