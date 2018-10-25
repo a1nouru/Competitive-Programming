c165a29ab6e3b2ed43ba0291f8a86803913be590
Nouru Muneza <nizzogentl@gmail.com>
2018-10-25 13:18:15 -0700

    LeetCode – Merge Intervals

---
+++ merge_Intervals.java
@@ -0,0 +1,30 @@
+public List<Interval> merge(List<Interval> intervals) {
+    List<Interval> result = new ArrayList<Interval>();
+ 
+    if(intervals==null||intervals.size()==0)
+        return result;
+ 
+    Collections.sort(intervals, new Comparator<Interval>(){
+        public int compare(Interval i1, Interval i2){
+            if(i1.start!=i2.start)
+                return i1.start-i2.start;
+            else
+                return i1.end-i2.end;
+        }
+    });
+ 
+    Interval pre = intervals.get(0);
+    for(int i=0; i<intervals.size(); i++){
+        Interval curr = intervals.get(i);
+        if(curr.start>pre.end){
+            result.add(pre);
+            pre = curr;
+        }else{
+            Interval merged = new Interval(pre.start, Math.max(pre.end, curr.end));
+            pre = merged;
+        }
+    }
+    result.add(pre);
+ 
+    return result;
+}
