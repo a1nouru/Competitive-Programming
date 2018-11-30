510606943ec057b9e47918f6ae4c7d3ed6a9d764
Nouru Muneza <nizzogentl@gmail.com>
2018-11-30 12:54:52 -0800

    56. Merge Intervals

--- mergeInterval.java
+++ mergeInterval.java
@@ -24,19 +24,25 @@ Explanation: Intervals [1,4] and [4,5] are considered overlapping.
  * }
  */
 class Solution {
-     public List<Interval> merge(List<Interval> intervals) {
-        intervals.sort((i1, i2) -> i1.start - i2.start); //sort to order by interval.start to ascending
-        int i = 0;
-        while (i < intervals.size() - 1) {
-            Interval curr = intervals.get(i);
-            Interval next = intervals.get(i + 1);    
-            if (next.start <= curr.end) { //if intersection 
-                curr.end = Math.max(curr.end, next.end); //keep max end between curr and next 
-                intervals.remove(i + 1); 
-            } else {
-                i++;
-            }
-        }
-        return intervals;
-    }
+    public List<Interval> merge(List<Interval> intervals) {
+	// sort start&end
+	int n = intervals.size();
+	int[] starts = new int[n];
+	int[] ends = new int[n];
+	for (int i = 0; i < n; i++) {
+		starts[i] = intervals.get(i).start;
+		ends[i] = intervals.get(i).end;
+	}
+	Arrays.sort(starts);
+	Arrays.sort(ends);
+	// loop through
+	List<Interval> res = new ArrayList<Interval>();
+	for (int i = 0, j = 0; i < n; i++) { // j is start of interval.
+		if (i == n - 1 || starts[i + 1] > ends[i]) {
+			res.add(new Interval(starts[j], ends[i]));
+			j = i + 1;
+		}
+	}
+	return res;
+}
 }
