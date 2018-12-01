da179713da52ee80e41c385535a430cf80fbca68
Nouru Muneza <nizzogentl@gmail.com>
2018-12-01 12:45:19 -0800

    79. word Search ||

--- word_search.java
+++ word_search.java
@@ -29,3 +29,74 @@ public static boolean exist(char [][] arr, String word){
         }
         return false;
     }
+
+
+//Extended part of the problem 
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+  // Determine if we can find the input word in the grid by taking a
+  // path through adjacent letters.
+  public static boolean findWord(char[][] grid, String word) {
+    
+    if (grid.length == 0 && grid[0].length == 0) return false; // base case
+    
+    for(int i = 0; i < grid.length; i++){
+    for(int j = 0; j < grid[0].length; j++){
+     if(word.charAt(0) == grid[i][j]){ //finding first char of word
+       if(dfs(grid,word,i , j, 0))
+         return true;
+     }
+  }
+  }
+    return false;
+  }
+  
+  public static boolean dfs(char [][] grid, String tempStr,int i, int j,int k){
+    int m = grid.length;
+    int n= grid[0].length;
+    
+    if(i <0 || j< 0 || i >= m || j >= n){ //checking out of bounds
+      return false;
+    }
+    
+    // String out of bounds 
+    if(k >= tempStr.length()){
+      return false;
+    }
+    
+     //if char index not match with grid[i][j] 
+    if(grid[i][j] != tempStr.charAt(k)){
+      return false;
+    }
+    
+    //we know that we have come to the end of our rec call and found string. 
+    if(k == tempStr.length()-1){
+        return true;
+    }
+    else if(dfs(grid,tempStr,i-1,j,k+1) //going left
+                || dfs(grid,tempStr,i+1,j,k+1) //going right
+                || dfs(grid,tempStr,i,j-1,k+1) //going up
+                || dfs(grid,tempStr,i,j+1,k+1)){ //going down 
+                return true;
+    }
+        return false;
+  }
+  
+  public static void main(String[] args) {
+    
+    char[][] testGrid = {{'c', 'b', 'd', 'k'},
+                         {'e', 'a', 'q', 'c'},
+                         {'d', 't', 't', 'a'},
+                         {'z', 'w', 'b', 'i'}};
+
+    String[] testStrings = {"cat", "bike", "bat","cax", "ci", "cxxxxxxxa"};
+    for (String s : testStrings) {
+      findWord(testGrid, s);
+      
+      System.out.println(findWord(testGrid, s));
+    }
+  }
+}
