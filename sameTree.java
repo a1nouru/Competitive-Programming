3b7e83980ba3d69d488d396e1f7f9d20d50c29e9
Nouru Muneza <nizzogentl@gmail.com>
2018-12-01 18:09:28 -0800

    100. Same Tree

---
+++ sameTree.java
@@ -0,0 +1,64 @@
+
+
+/*
+
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean dfs(TreeNode p, TreeNode q){
+    if(p == null && q == null){
+        return true;
+    } else if (p == null && q != null){
+        return false;
+    } else if(q == null && p != null){
+        return false;
+    }
+    return dfs(p.left, q.left) && dfs(p.right, q.right) && p.val == q.val;
+}
+
+public boolean isSameTree(TreeNode p, TreeNode q) {
+    return dfs(p, q);
+}
+}
+
