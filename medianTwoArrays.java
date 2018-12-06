1418c15f19d90f5f380c3133442fbdf2772c3837
Nouru Muneza <nizzogentl@gmail.com>
2018-12-06 12:25:19 -0800

    4. Median of Two Sorted Arrays

--- medianTwoArrays.java
+++ medianTwoArrays.java
@@ -1,31 +1,51 @@
-public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-    int total = nums1.length+nums2.length;
-    if(total%2==0){
-        return (findKth(total/2+1, nums1, nums2, 0, 0)+findKth(total/2, nums1, nums2, 0, 0))/2.0;
-    }else{
-        return findKth(total/2+1, nums1, nums2, 0, 0);
-    }
-}
- 
-public int findKth(int k, int[] nums1, int[] nums2, int s1, int s2){
-    if(s1>=nums1.length)
-        return nums2[s2+k-1];
- 
-    if(s2>=nums2.length)
-        return nums1[s1+k-1];
- 
-    if(k==1)
-        return Math.min(nums1[s1], nums2[s2]);
- 
-    int m1 = s1+k/2-1;
-    int m2 = s2+k/2-1;
- 
-    int mid1 = m1<nums1.length?nums1[m1]:Integer.MAX_VALUE;    
-    int mid2 = m2<nums2.length?nums2[m2]:Integer.MAX_VALUE;
- 
-    if(mid1<mid2){
-        return findKth(k-k/2, nums1, nums2, m1+1, s2);
-    }else{
-        return findKth(k-k/2, nums1, nums2, s1, m2+1);
-    }
+/*
+
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+You may assume nums1 and nums2 cannot be both empty.
+
+Example 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+*/
+
+//Explanation
+
+//The key point of this problem is to ignore half part of A and B each step recursively by comparing the median of remaining A and B:
+/*
+if (aMid < bMid) Keep [aRight + bLeft]    
+else Keep [bRight + aLeft]
+As the following: time=O(log(m + n))
+*/
+public double findMedianSortedArrays(int[] A, int[] B) {
+	    int m = A.length, n = B.length;
+	    int l = (m + n + 1) / 2;
+	    int r = (m + n + 2) / 2;
+	    return (getkth(A, 0, B, 0, l) + getkth(A, 0, B, 0, r)) / 2.0;
+	}
+
+public double getkth(int[] A, int aStart, int[] B, int bStart, int k) {
+	if (aStart > A.length - 1) return B[bStart + k - 1];            
+	if (bStart > B.length - 1) return A[aStart + k - 1];                
+	if (k == 1) return Math.min(A[aStart], B[bStart]);
+	
+	int aMid = Integer.MAX_VALUE, bMid = Integer.MAX_VALUE;
+	if (aStart + k/2 - 1 < A.length) aMid = A[aStart + k/2 - 1]; 
+	if (bStart + k/2 - 1 < B.length) bMid = B[bStart + k/2 - 1];        
+	
+	if (aMid < bMid) 
+	    return getkth(A, aStart + k/2, B, bStart,       k - k/2);// Check: aRight + bLeft 
+	else 
+	    return getkth(A, aStart,       B, bStart + k/2, k - k/2);// Check: bRight + aLeft
 }
