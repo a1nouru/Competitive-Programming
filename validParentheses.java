c2327dd0f29995e0e0bed571fe1608108d294e73
Nouru Muneza <nizzogentl@gmail.com>
2018-12-05 22:12:14 -0800

    20. Valid Parentheses

---
+++ validParentheses.java
@@ -0,0 +1,50 @@
+/*
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true
+*/
+
+class Solution {
+    public boolean isValid(String s) {
+      Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c) //stack.pop() != c because if it's not openning then 
+    //it should be the most recent closing and if stack.pop() != c then we know it's not corresponding closing 
+    //bracket or parathesis. Check also works for "]]}}(("
+			return false;
+	}
+	return stack.isEmpty(); 
+    }
+}
