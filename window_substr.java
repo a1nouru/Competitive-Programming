d8301d140c142c94c8335cf984c81b5d546e3717
Nouru Muneza <nizzogentl@gmail.com>
2019-05-06 14:31:35 -0700

    Minimum Window Substring

--- window_substr.java
+++ window_substr.java
@@ -1,56 +1,30 @@
-public String minWindow(String s, String t) {
-    if(t.length()>s.length()) 
-        return "";
-    String result = "";
- 
-    //character counter for t
-    HashMap<Character, Integer> target = new HashMap<Character, Integer>();
-    for(int i=0; i<t.length(); i++){
-        char c = t.charAt(i);    
-        if(target.containsKey(c)){
-            target.put(c,target.get(c)+1);
-        }else{
-            target.put(c,1);  
+class Solution{
+    public String minWindow(String s, String t){
+        int [] table = new int ['z'+1];
+        int counter = t.length(), begin = 0, end = 0, len = Integer.MAX_VALUE;
+        String ans = "";
+        for (char c : t.toCharArray()){ //calculating frequencies. 
+            table[c]++;
         }
-    }
- 
-    // character counter for s
-    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-    int left = 0;
-    int minLen = s.length()+1;
- 
-    int count = 0; // the total of mapped characters
- 
-    for(int i=0; i<s.length(); i++){
-        char c = s.charAt(i);
- 
-        if(target.containsKey(c)){
-            if(map.containsKey(c)){
-                if(map.get(c)<target.get(c)){
-                    count++;
+
+        while (end < s.length()){
+            char endChar = s.charAt(end++);
+            
+            if(table[endChar] > 0) counter--; // if char if found in t and just got covered in current slidding window. 
+            table[endChar]--;
+            
+            while(counter == 0){ //if all chars in t are covered in slidding window 
+                if(end-begin < len){ // calc the new min window length 
+                    len = end -begin; 
+                    ans = s.substring(begin, end);
                 }
-                map.put(c,map.get(c)+1);
-            }else{
-                map.put(c,1);
-                count++;
+                
+                char beginChar = s.charAt(begin++);
+                if (table[beginChar] ==0) counter++; //window cannot start with a char not from t. so counter ++ to indicate that there is a missing char in current window from t or duplicate if all chars are included. 
+                table[beginChar]++; //removing from slidding window. 
             }
         }
- 
-        if(count == t.length()){
-            char sc = s.charAt(left);
-            while (!map.containsKey(sc) || map.get(sc) > target.get(sc)) {
-                if (map.containsKey(sc) && map.get(sc) > target.get(sc))
-                    map.put(sc, map.get(sc) - 1);
-                left++;
-                sc = s.charAt(left);
-            }
- 
-            if (i - left + 1 < minLen) {
-                result = s.substring(left, i + 1);
-                minLen = i - left + 1;
-            }
-        }
+        return ans;
     }
- 
-    return result;
 }
+
