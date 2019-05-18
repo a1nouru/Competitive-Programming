24a171c0ce389b11c11ceb5cb3278d4391fa7fae
Nouru Muneza <nizzogentl@gmail.com>
2019-05-18 13:14:21 -0700

    42. Trapping Rain Water

--- trapRainWater.java
+++ trapRainWater.java
@@ -11,24 +11,15 @@ Input: [0,1,0,2,1,0,1,3,2,1,2,1]
 Output: 6
 
 */
-
 class Solution {
-    
     //How this works: 
-    
     /*
-    water can only be trapped if the current index is under both left and right max(a valley). 
-    The solution also calculates both valleys from left and from right depending 
-    on if(height[p1] < height[p2]) 
+    The desired area is a valley. 
+    We have curr p1 and p2 which are indexes from left p1 and p2 from right. 
+    we can only have a valey on the left if p1 < p2 and p1 < lmax and then we calculate water trapped as lmax - arr[p1]
     
-    if(height[p1] < lmax)  means height[p1] is less than lMax and also height[p1] < height[p2] 
-    which might be rmax or less than rmax, so either way we lmax is the min compared to rmax and height[p2]
-    
-    So basically we are calculating water traps both from right and left using current height[p1]/ left and 
-    height[p2] / right while making sure that both current are under lmax and rmax. 
+    //Same applies to p2 from the right. 
     */
-    
-    
      public int trap(int[] height) {
         int p1 = 0, p2 = height.length-1, total =0,
         lmax = 0, rmax = 0;
