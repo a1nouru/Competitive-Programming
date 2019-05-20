004a1313653afcf961a6255853c7b2078223578e
Nouru Muneza <nizzogentl@gmail.com>
2019-05-20 16:24:10 -0700

    79. Word Search

--- word_search.java
+++ word_search.java
@@ -1,102 +1,49 @@
-public static boolean exist(char [][] arr, String word){
-        if (arr.length == 0 && arr[0].length == 0) return false; // base case
+/*
+Given a 2D board and a word, find if the word exists in the grid.
 
-        int m = arr.length;
-        int n = arr[0].length;
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
 
-        for (int i = 0; i< m; i++){
-            for (int j = 0; j < n; j++){
-                if (DFS(arr,word,i,j,0)) return true;
-            }
-        }
-        return false;
-    }
+Example:
 
-    private static boolean DFS(char[][] arr,String word, int i, int j, int k){
-        int m = arr.length;
-        int n = arr[0].length;
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
 
-        if (i < 0 || j < 0 || i > m || j> n) //checking if out of bounds
-            return false;
-        if (arr[i][j] == word.charAt(k)){
-            if (k == word.length()-1) // we are at the end of the word and all the characters have been found.
-                return true;
-            else if( DFS(arr,word,i-1,j,k+1) //going left
-                || DFS(arr,word,i+1,j,k+1) //going right
-                || DFS(arr,word,i,j-1,k+1) //going down
-                || DFS(arr,word,i,j+1,k+1)) //going up
+Given word = "ABCCED", return true.
+Given word = "SEE", return true.
+Given word = "ABCB", return false.
+*/
+class Solution{ 
+   public boolean exist(char[][] board, String word){
+    if (board[0].length == 0 || board.length == 0 || word.length() == 0) return false; 
+    int n = board[0].length, m = board.length;
+    for (int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            if (board[i][j] == word.charAt(0) && DFS(board, word, i, j, 0, new boolean[board.length][board[i].length]))
                 return true;
         }
-        return false;
     }
-
-
-//Extended part of the problem 
-
-import java.io.*;
-import java.util.*;
-
-class Solution {
-  // Determine if we can find the input word in the grid by taking a
-  // path through adjacent letters.
-  public static boolean findWord(char[][] grid, String word) {
-    
-    if (grid.length == 0 && grid[0].length == 0) return false; // base case
-    
-    for(int i = 0; i < grid.length; i++){
-    for(int j = 0; j < grid[0].length; j++){
-     if(word.charAt(0) == grid[i][j]){ //finding first char of word
-       if(dfs(grid,word,i , j, 0))
-         return true;
-     }
-  }
-  }
     return false;
-  }
-  
-  public static boolean dfs(char [][] grid, String tempStr,int i, int j,int k){
-    int m = grid.length;
-    int n= grid[0].length;
-    
-    if(i <0 || j< 0 || i >= m || j >= n){ //checking out of bounds
-      return false;
-    }
-    
-    // String out of bounds 
-    if(k >= tempStr.length()){
-      return false;
-    }
-    
-     //if char index not match with grid[i][j] 
-    if(grid[i][j] != tempStr.charAt(k)){
-      return false;
-    }
-    
-    //we know that we have come to the end of our rec call and found string. 
-    if(k == tempStr.length()-1){
-        return true;
-    }
-    else if(dfs(grid,tempStr,i-1,j,k+1) //going left
-                || dfs(grid,tempStr,i+1,j,k+1) //going right
-                || dfs(grid,tempStr,i,j-1,k+1) //going up
-                || dfs(grid,tempStr,i,j+1,k+1)){ //going down 
-                return true;
-    }
-        return false;
-  }
-  
-  public static void main(String[] args) {
-    
-    char[][] testGrid = {{'c', 'b', 'd', 'k'},
-                         {'e', 'a', 'q', 'c'},
-                         {'d', 't', 't', 'a'},
-                         {'z', 'w', 'b', 'i'}};
+}
 
-    String[] testStrings = {"cat", "bike", "bat","cax", "ci", "cxxxxxxxa"};
-    for (String s : testStrings) {
-      findWord(testGrid, s);
-      
-      System.out.println(findWord(testGrid, s));
-    }
-  }
+public boolean DFS (char [][] board,String word, int i, int j, int k, boolean [][] visited){
+    if (k == word.length()) return true; 
+    
+    if(i < 0 || j < 0 || j >=board[0].length 
+       || i >= board.length || word.charAt(k) != board[i][j] || visited[i][j]) 
+        return false;
+        
+    visited[i][j] = true;
+    if(DFS(board, word, i-1, j, k+1, visited) || 
+       DFS(board, word, i+1, j, k+1, visited) || 
+       DFS(board, word, i, j-1, k+1, visited) ||
+       DFS(board, word, i, j+1, k+1, visited))
+        return true;
+    
+    visited[i][j] = false;
+    return false;
+}
 }
