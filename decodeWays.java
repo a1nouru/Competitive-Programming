b3acc6a4a057f402d8cfda69f2a88521754cc91d
Nouru Muneza <nizzogentl@gmail.com>
2019-05-21 10:04:05 -0700

    91. Decode Ways

--- decodeWays.java
+++ decodeWays.java
@@ -19,41 +19,21 @@ Input: "226"
 Output: 3
 Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 */
-
-class Solution {
+class Solution { 
     public int numDecodings(String s) {
-        if(s == null || s.length() == 0) return 0;
-        // Index to NumOfWays mapping
-        Map<Integer, Integer> memo = new HashMap<Integer, Integer>(); //Map to keep pre calculated totals. 
-        return dfs(s, 0, memo);
-    }
-    
-    private int dfs(String s, int start, Map<Integer, Integer> memo){
-        if(start > s.length() + 1) return 0; //start goes beyond s.length()
-        if(start == s.length()){ // we are at end of string
-            return 1;
-        } 
-                    
-        int total = 0;
+        if (s == null || s.lengthf() == 0) return 0;
+        int n = s.length();
+        int [] dp = new int [n+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
         
-        if(memo.containsKey(start)) return memo.get(start); //if total already calculted at a previsited startIndex. (DONT RE-CALCULATE)
-        
-        //choose single digit
-        String currStr = s.substring(start, start + 1);
-        Integer currInt = Integer.valueOf(currStr);
-        if(currInt == 0) return 0;
-        if(currInt >= 1 && currInt <= 26)
-            total = dfs(s, start + 1, memo);
-
-        //choose two digits (if possible)
-        if(start < s.length() - 1){  //if start + 2 if within limit 
-            currStr = s.substring(start, start + 2);
-            currInt = Integer.valueOf(currStr);
-            if(currInt >= 1 && currInt <= 26)
-                total += dfs(s, start + 2, memo); //add pick 2 char rec call to pick 1 char. 
+        for(int i = 0; i <= n; i++){
+            if(s.charAt(0) != '0') 
+                dp[i] = dp[i-1];
+            int twoDigits = s.substring(i-2, i);
+            if(twoDigits >= 10 && twoDigits <= 26)
+                dp[i] += dp[i-2];
         }
-        
-        memo.put(start, total); //keep pre calculated totals at startIndex
-        return total; //return total to calling rec call
     }
 }
+
