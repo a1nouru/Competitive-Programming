8fab309dd382a1dcb72167cba1d12d85e72e459f
Nouru Muneza <nizzogentl@gmail.com>
2019-05-23 16:43:47 -0700

    39. Combination Sum

--- combinationSum.java
+++ combinationSum.java
@@ -26,32 +26,27 @@ A solution set is:
 ]
 */
 
-
-class Solution {
-    public List<List<Integer>> combinationSum (int[] candidates, int target){
-    Arrays.sort(candidates);
-
-    List<List<Integer>> results = new ArrayList<>();
-    combinationSumHelper(results, new ArrayList<>(), candidates, target, 0);
-    return results;
+class Solution{
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int [] num, int target){
+        if (num == null  || num.length == 0) return res;
+        Arrays.sort(num);
+        helper(num, target, 0, new ArrayList<Integer>(), res);
+        return res;
     }
-
-    public void combinationSumHelper(List<List<Integer>> results, List<Integer> tempResult, int[] candidates,int target,
-                                     int start) {
-        if (target == 0) {
-        results.add(new ArrayList<>(tempResult));
-            return;
+    
+    public void helper(int [] num, int target, int indx, List<Integer>temp, List<List<Integer>> res){
+        if(target == 0) {
+            res.add(new ArrayList<>(temp));
+            return; 
         }
-        for (int i=start; i<candidates.length; i++) {
-        if (candidates[i] > target) {
-            break;
-        }
-        if (i != 0 && candidates[i] == candidates[i - 1]) { //duplicates 
-            continue;
-        }
-        tempResult.add(candidates[i]);
-        combinationSumHelper(results, tempResult, candidates, target - candidates[i], i);
-        tempResult.remove(tempResult.size()-1);
+        if(target < 0) return;
+        for(int i = indx; i < num.length; i++){
+            temp.add(num[i]);
+            helper(num, target-num[i], i, temp, res);
+            temp.remove(temp.size()-1);
         }
     }
 }
+
+
