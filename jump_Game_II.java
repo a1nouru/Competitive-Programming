fbcf10945a39d465ad8dc694610084b09ebf0604
Nouru Muneza <nizzogentl@gmail.com>
2019-05-22 19:34:44 -0700

    45. Jump Game II

--- jump_Game_II.java
+++ jump_Game_II.java
@@ -11,9 +11,7 @@ Example:
 Input: [2,3,1,1,4]
 Output: 2
 Explanation: The minimum number of jumps to reach the last index is 2.
-    Jump 1 step from index 0 to 1, then 3 steps to the last index.
-    
-*/
+    Jump 1 step from index 0 to 1, then 3 steps to the last index.*/
 
 class Solution {
     /*The main idea is based on greedy. Let's say the range of the current jump is [curBegin, curEnd], curFarthest is the farthest point that all points in [curBegin, curEnd] can reach. Once the current point reaches curEnd, then trigger another jump, and set the new curEnd with curFarthest, then keep the above steps, as the following:
