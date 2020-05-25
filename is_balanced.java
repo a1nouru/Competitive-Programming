397f819506f6dc7840489fd8bafcad0b5a7e9917
Nouru Muneza <nizzogentl1@gmail.com>
2020-05-25 11:31:05 -0500

    Balanced Binary Tree

--- is_balanced.java
+++ is_balanced.java
@@ -1,41 +1,39 @@
-  
-//Time complexity: Big O(n^2) where n is the number of nodes in the tree. 
-int getHeight(TreeNode root){
-            if(root == null) return -1;
-  
-            return Math.max(getHeight(root.left), getHeight(root.right))+1; // find height of this subtree
-        }
+/* 
+Given a binary tree, determine if it is height-balanced.
 
-        boolean is_balanced(TreeNode root){
-            if(root == null) //base case
-                return true;
-            int heightDiff = (getHeight(root.left) - getHeight(root.right));
+For this problem, a height-balanced binary tree is defined as:
 
-            if(Math.abs(heightDiff) > 1){ //check if height is >1
-                return false;
-            }
-            else
-                return is_balanced(root.left) && is_balanced(root.right); > //recursive call to move down the tree while checking all heights
-        }
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+Example 1:
+
+Given the following tree [3,9,20,null,null,15,7]:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Return true.
+
+*/
 
 
-// A more optimized solution.
-
-//Here the idea is to subtract min dept(value returned by midepth) from Max depth and the we can find difference in height. 
-boolean isBalanced(TreeNode root){
-            if(root == null) throw new IllegalArgumentException("tree is empty");
-
-            return maxDepth(root) - minDepth(root) <=1;
-        }
-
-        int maxDepth(TreeNode root){ //finding max depth of the root
-            if (root == null) return 0;
-
-            return Math.max(maxDepth(root.left), maxDepth(root.right)) +1;
-        }
-
-        int minDepth(TreeNode root){ //finding min depth of the root 
-            if (root == null) return 0;
-            
-            return Math.min(minDepth(root.left), minDepth(root.right))+1;
-        }
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+       
+        return depth(root) != -1;
+    }
+    
+    public int depth(TreeNode root){
+       if (root == null) return 0;
+        
+        int left = depth(root.left);
+        int right = depth(root.right);
+        
+        if (left == -1 || right == -1 || Math.abs(left-right) > 1)
+            return -1;
+        
+        return Math.max(left, right) + 1;
+    }
+}
