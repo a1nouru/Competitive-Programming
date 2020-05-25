82d9c3a0f766254b350e1f4cd4ccb2c1faeeb160
Nouru Muneza <nizzogentl1@gmail.com>
2020-05-25 15:48:16 -0500

    Increasing Order Search Tree

---
+++ increasingTree.java
@@ -0,0 +1,50 @@
+/*
+Given a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only 1 right child.
+
+Example 1:
+Input: [5,3,6,2,4,null,8,1,null,null,null,7,9]
+
+       5
+      / \
+    3    6
+   / \    \
+  2   4    8
+ /        / \ 
+1        7   9
+
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+
+ 1
+  \
+   2
+    \
+     3
+      \
+       4
+        \
+         5
+          \
+           6
+            \
+             7
+              \
+               8
+                \
+                 9  
+*/
+
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        return increasingBST(root, null);
+    }
+
+    public TreeNode increasingBST(TreeNode root, TreeNode next) {
+        if (root == null) return next;
+        
+        TreeNode res = increasingBST(root.left, root);
+        root.left = null;
+        root.right = increasingBST(root.right, next);
+        
+        return res;
+    }
+}
